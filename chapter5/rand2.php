b0733ee5a6ad63c2ab9549334f1cb1e9bd99db96
HP016\HP016 <ue2768308@gmail.com>
2026-04-02 11:16:56 +0900

    chapter5リセット

--- chapter5/rand2.php
+++
@@ -1,5 +0,0 @@
-<?php require '../header.php'; ?>
-<?php
-echo rand(1, 6);
-?>
-<?php require '../footer.php'; ?>
--- chapter5/rand3.php
+++
@@ -1,5 +0,0 @@
-<?php require '../header.php'; ?>
-<?php
-echo '<img alt="image" src="item', rand(0, 2), '.png">';
-?>
-<?php require '../footer.php'; ?>
