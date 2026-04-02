1b1a5b1beb2b4de79de1bd396c05fe8cbb82579f
HP016\HP016 <ue2768308@gmail.com>
2026-04-02 10:51:05 +0900

    checksファイルに記述

--- chapter4/checks-input.php
+++ chapter4/checks-input.php
@@ -1,15 +1,16 @@
 <?php require '../header.php'; ?>
 <p>ご興味のある商品ジャンルを全て選んでください。</p>
 <form action="checks-output.php" method="post">
-<?php
-$genre=['カメラ', 'パソコン', '時計', '家電', '書籍', '文房具', '食品'];
-foreach ($genre as $item) {
-	echo '<p>';
-	echo '<input type="checkbox" name="genre[]" value="', $item, '">';
-	echo $item;
-	echo '</p>';
-}
-?>
-<p><input type="submit" value="確定"></p>
+	<?php 
+	$genre=['カメラ','パソコン','時計','家電','書籍','文房具','食品',];
+	foreach ($genre as $item) {
+		echo '<p>';
+		// チェックボックスにしているため、name属性は配列形式にする必要がある。
+		echo '<input type="checkbox" name="genre[]" value="', $item, '">';
+		echo $item;
+		echo '</p>';
+	}
+	?>
+	<p><input type="submit" value="確定"></p>
 </form>
 <?php require '../footer.php'; ?>
